53d170e02a8e7832a194a718a41352b58b5b7653
tawankub <158823103+tawankub@users.noreply.github.com>
2024-05-13 22:31:16 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint=
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khao Yai Netional Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50404083853113,
+            14.333843367830141
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand palace",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49266868905255,
+            13.751516205984203
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "icon siam",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.51043649999929,
+            13.726690655187085
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ko Kham",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.93363981348853,
+            12.573599528471306
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chiang Mai",
+          "type": "city",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.98554042032765,
+            18.788548049114212
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Cha am",
+          "type": "cutural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.96703600471596,
+            12.79969518280214
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Pasak Jolasit Dam",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.06572749316564,
+            14.861400963897438
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Pho",
+          "type": "cutural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.49330445767504,
+            13.74640676006554
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Deam World",
+          "type": "spacial",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.67754449623283,
+            13.991394658464728
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phu Kradueng National Park",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.7595482261932,
+            16.893503187467232
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
